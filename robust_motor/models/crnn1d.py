4ead00ddb45893e0cb922b81639316a28c895e6f
sagarverma <sagar15056@iiitd.ac.in>
2022-06-21 06:35:39 +0200

    big

--- robust_motor/models/crnn1d.py
+++ robust_motor/models/crnn1d.py
@@ -12,14 +12,18 @@ class CRNN(nn.Module):
         self.out_channels = out_channels
 
         # (batch, channels, length)
-        self.cnn = nn.Conv1d(in_channels=self.in_channels, 
+        self.cnn1 = nn.Conv1d(in_channels=self.in_channels, 
                             out_channels=self.out_channels, 
                             kernel_size=16, 
                             stride=2)
+        self.cnn2 = nn.Conv1d(in_channels=self.out_channels, 
+                            out_channels=self.out_channels, 
+                            kernel_size=8, 
+                            stride=1)
         # (batch, seq, feature)
         self.rnn = nn.LSTM(input_size=(self.out_channels), 
                             hidden_size=self.out_channels, 
-                            num_layers=2, 
+                            num_layers=1, 
                             batch_first=True, 
                             bidirectional=False)
         self.dense = nn.Linear(out_channels, n_classes)
@@ -34,7 +38,8 @@ class CRNN(nn.Module):
         out = out.permute(0,2,1)
         out = out.view(-1, self.n_len_seg, self.n_channel)
         out = out.permute(0,2,1)
-        out = self.cnn(out)
+        out = self.cnn1(out)
+        out = self.cnn2(out)
         out = out.mean(-1)
         out = out.view(-1, self.n_seg, self.out_channels)
         _, (out, _) = self.rnn(out)
